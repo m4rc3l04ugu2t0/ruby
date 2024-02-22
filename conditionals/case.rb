b5cf0b46893b5561e89b64aac7b876fac7c870d8
Marcelo Augusto <mrcelo014@gmail.com>
2024-02-22 16:32:28 -0300

    symbols

---
+++ conditionals/case.rb
@@ -0,0 +1,12 @@
+month = "January"
+
+case month
+when "January"
+    puts "31 days"
+    when "February"
+        puts "28 days"
+        when "March"
+            puts "30 days"
+        else 
+            puts "I don't know how many days it is"
+            end
---
+++ symbols/symbols.rb
@@ -0,0 +1,20 @@
+# Definindo símbolos
+:symbol
+:"another symbol"
+:"symbol_with_underscores"
+:"123"
+
+# Usando símbolos como chaves em hashes
+user = { name: "Alice", age: 30 }
+
+# Comparando símbolos
+puts :symbol == :symbol  # true
+puts :symbol == "symbol" # false, pois um é símbolo e o outro é string
+
+# Convertendo entre símbolos e strings
+puts :symbol.to_s        # "symbol"
+puts "symbol".to_sym     # :symbol
+
+# Verificando a presença de um símbolo em um array
+symbols = [:apple, :banana, :cherry]
+puts symbols.include?(:banana) # true
