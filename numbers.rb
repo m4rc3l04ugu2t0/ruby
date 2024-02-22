2cd901e598969b0ed2e74aa83228eea551d7e2d5
Marcelo Augusto <mrcelo014@gmail.com>
2024-02-22 11:54:01 -0300

    numbers

---
+++ numbers.rb
@@ -0,0 +1,14 @@
+puts "Enter a number"
+x = gets.chomp.to_f
+puts "Enter other number"
+y = gets.chomp.to_f
+
+sum = x + y
+subt = x - y
+div = x / y
+mut = x * y
+
+puts "#{x} + #{y} is #{sum}"
+puts "#{x} - #{y} is #{subt}"
+puts "#{x} / #{y} is #{div}"
+puts "#{x} * #{y} is #{mut}"
