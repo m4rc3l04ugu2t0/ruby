65f7872b392707f3e4983617afefb7f59675bca6
Marcelo Augusto <mrcelo014@gmail.com>
2024-02-23 17:36:32 -0300

    def calc

---
+++ methods/calc.rb
@@ -0,0 +1,16 @@
+def calc x, y, operation
+
+    case operation
+        when "sum"
+            x + y
+   
+        when "sub"
+            x - y
+        when "mult"
+            x * y
+        when "div"
+            x / y
+    end
+end
+
+puts calc 3, 3, "div"
